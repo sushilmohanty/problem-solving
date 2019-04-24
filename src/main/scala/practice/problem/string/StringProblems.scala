243e5c30834868d78b5fc448f388676f6d83d4c0
sushil <sushil.mohanty@mediacorp.com.sg>
2019-04-24 10:17:43 +0800

    Solution to sample  string problems

---
+++ src/main/scala/practice/problem/string/StringProblems.scala
@@ -0,0 +1,101 @@
+package practice.problem.string
+
+import scala.collection.mutable
+import scala.util.control.Breaks.{break, breakable}
+
+
+object StringProblems {
+
+  /**
+    * Given two strings check if one is rotation of other
+    * eg : bar, arb, rba
+    *
+    * @param str1
+    * @param str2
+    * @return
+    */
+
+  def checkStringRotation(str1 : String, str2 : String) : Boolean = {
+    // Concat both the strings
+    // For a valid rotation second string must present as substring
+    var isRotation = false
+    if(str1.length == str2.length) {
+      isRotation = (str1.concat(str2)).contains(str2)
+    }
+    isRotation
+  }
+
+  /**
+    * Get max occurred char count in a string
+    * Asumption : All chars are ASCII
+    *
+    * @param str
+    * @return
+    */
+
+  def getMaxCharCountInString(str : String): Int = {
+    val stringArray = str.toCharArray
+    val lookupArray = Array.fill[Int](256)(0)
+    var currMax = 0
+    stringArray.foreach(char => {
+      lookupArray(char.toInt) += 1
+      if(currMax < lookupArray(char.toInt)) {
+        currMax = lookupArray(char.toInt)
+      }
+    })
+    currMax
+  }
+
+
+  /**
+    * Remove duplicates from a String
+    *
+    * @param str
+    * @return
+    */
+  def removeDuplicatesFromString(str : String): String = {
+    val stringArray = str.toCharArray
+    val set = new mutable.HashSet[Char]()
+    val strDedupled = new mutable.StringBuilder()
+    stringArray.foreach(char => {
+      if (!set.contains(char)) {
+        set.add(char)
+        strDedupled.append(char)
+      }
+    })
+    strDedupled.toString()
+  }
+
+
+  /**
+    *
+    * Check if a palindrome String can be constructed
+    * using all the characters in the input String.
+    *
+    * @param str
+    * @return
+    */
+  def checkPalindrome(str : String) : Boolean = {
+    val stringArray = str.toCharArray
+    val lookupArray = Array.fill[Int](256)(0)
+    var oddOccourance = 0
+    var isPalindrome = true
+    stringArray.foreach(char => {
+      lookupArray(char.toInt) += 1
+    })
+    breakable {
+      lookupArray.foreach(element => {
+        if (element == 1) oddOccourance += 1
+        if (oddOccourance > 1 )  {
+          isPalindrome = false
+          break
+        }
+      })
+    }
+    return  isPalindrome
+  }
+
+
+
+
+}
---
+++ src/main/scala/practice/problems/euler/ProjectEuler1.scala
@@ -0,0 +1,9 @@
+package practice.problems.euler
+
+class ProjectEuler1 {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
